d42a8a43b8fc3dcdc7e537010f7bdbc88ca69c4b
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-09-02 17:46:09 +0530

    added AppHeader, Search & Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import AppHeader from "./components/AppHeader";
+import Search from "./components/Search";
+import Card from "./components/Card";
 
 class App extends Component {
+  state = {
+    results: [],
+    resultLen: null
+  };
+
+  getResults = results => {
+    if (results.length === 0) {
+      this.setState({
+        resultLen: 0
+      });
+    } else {
+      this.setState({
+        resultLen: null
+      });
+    }
+
+    this.setState({
+      results
+    });
+  };
+
   render() {
     return (
-      <div id="search-app">
-        <h1>iTunes Search App</h1>
+      <div id="search-app" className="bg-light">
+        <AppHeader />
+        <div className="container">
+          <Search getResults={this.getResults} />
+          <Card results={this.state.results} />
+          {this.state.resultLen === 0 && (
+            <h4 className="text-center alert alert-danger m-5">
+              Sorry No Results !
+            </h4>
+          )}
+        </div>
       </div>
     );
   }
