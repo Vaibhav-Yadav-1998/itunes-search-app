54e6513343df5d33217ebf263f4139736dedb358
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-09-02 17:46:36 +0530

    created AppHeader

---
+++ src/components/AppHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppHeader = () => (
+  <nav className="navbar navbar-dark bg-dark">
+    <a href="#!" className="navbar-brand">
+      iTunes Search
+    </a>
+  </nav>
+);
+
+export default AppHeader;
